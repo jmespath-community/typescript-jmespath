3862c5f86a8986fcd7647c0f47ed07ea95f7dd69
Springcomp <springcomp@users.noreply.github.com>
2023-03-07 13:35:20 +0100

    fix: unit tests.

--- test/jmespath-functions.spec.ts
+++ test/jmespath-functions.spec.ts
@@ -2,6 +2,6 @@ import { search } from '../src';
 
 describe('Evaluates functions', () => {
   it('search a in lexical scope', () => {
-    expect(search({}, "let({foo: 'bar'}, &foo") ).toEqual('bar');
+    expect(search({}, "let({foo: 'bar'}, &foo)") ).toEqual('bar');
   });
 });
