d87c2dc6485794b6da00adc655c81c7d482b897d
Springcomp <springcomp@users.noreply.github.com>
2023-03-07 13:31:26 +0100

    fix: ts unit test.

--- test/jmespath-functions.spec.js
+++
@@ -1,8 +0,0 @@
-import { compile, TreeInterpreter } from '../src';
-
-describe('Evaluates functions', () => {
-  it('search a in lexical scope', () => {
-    const ast = compile("let({foo: 'bar'}, &foo)");
-    expect(TreeInterpreter.search(ast, {})).toEqual('bar');
-  });
-});
---
+++ test/jmespath-functions.spec.ts
@@ -0,0 +1,7 @@
+import { search } from '../src';
+
+describe('Evaluates functions', () => {
+  it('search a in lexical scope', () => {
+    expect(search({}, "let({foo: 'bar'}, &foo") ).toEqual('bar');
+  });
+});
